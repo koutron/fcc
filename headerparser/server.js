7e408f115962a87d063d2b206c610da067362513
koutron <kamousavi@gmail.com>
2017-12-16 18:17:04 -0500

    cont updated fcc titles

--- headerparser/server.js
+++ headerparser/server.js
@@ -3,17 +3,17 @@ var express = require('express');
 var ip = require('ip');
 var path = require('path');
 var router = express.Router();
-router.get('/info', function(req, res){
-var agent = useragent.parse(req.headers['user-agent']);
-  res.send(agent.toString() + '<br>' + ip.address());
-});
+
 
 router.use('/', express.static(path.join(__dirname, '../public')));
 router.get('/', function (request, response) {
   response.sendFile(__dirname + '/headerparser.html');
 });
 
-
+router.get('/info', function(req, res){
+var agent = useragent.parse(req.headers['user-agent']);
+  res.send(agent.toString() + '<br>' + ip.address());
+});
 
 //router.use(express.static('public'));
 //router.get("/", function (request, response) {
