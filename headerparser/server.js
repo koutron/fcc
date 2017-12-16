0cebd554edd5820001863e2664b558946972b47b
koutron <kamousavi@gmail.com>
2017-12-16 18:38:20 -0500

    cont

--- headerparser/server.js
+++ headerparser/server.js
@@ -4,23 +4,14 @@ var ip = require('ip');
 var path = require('path');
 var router = express.Router();
 
-router.get('./info', function(req, res){
-var agent = useragent.parse(req.headers['user-agent']);
-  res.send(agent.toString() + '<br>' + ip.address());
-});
-
 router.use('/', express.static(path.join(__dirname, '../public')));
 router.get('/', function (request, response) {
   response.sendFile(__dirname + '/headerparser.html');
 });
 
-
-
-//router.use(express.static('public'));
-//router.get("/", function (request, response) {
-//  response.sendFile(__dirname + '/views/index.html');
-//});
-
-
+router.get('info', function(req, res){
+var agent = useragent.parse(req.headers['user-agent']);
+  res.send(agent.toString() + '<br>' + ip.address());
+});
 
 module.exports = router;
